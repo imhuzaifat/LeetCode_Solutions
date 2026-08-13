8127dc45bcb9b48034e189f6a486aba1751494d6
Huzaifa Tahir <imhuzaifat@gmail.com>
2026-08-13 06:05:43 +0500

    Updated 0026 - Added Better Logic

--- 0026_remove_duplicates_from_sorted_array.cpp
+++ 0026_remove_duplicates_from_sorted_array.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
-        return nums.size();
+        int read = 1, write = 1;
+        while (read < nums.size())
+        {
+            if (nums[read] != nums[read-1])
+            {
+                nums[write] = nums[read];
+                write++;
+            }
+            read++;
+        }
+        return write;
     }
 };
