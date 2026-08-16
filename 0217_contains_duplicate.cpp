7f18d3e926cd5008c93bac4a088c03ae45ecb74e
Huzaifa Tahir <imhuzaifat@gmail.com>
2026-08-16 14:58:18 +0500

    Added 0217

---
+++ 0217_contains_duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        if (unique(nums.begin(), nums.end()) < nums.end())
+            return true;
+
+        return false;
+    }
+};
